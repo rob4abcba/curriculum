81446d102308fc781bb826b727e5625635a95daa
rob4abcba <rob_abcba@yahoo.com>
2020-05-16 16:59:41 -0700

    JS1 Closures Ex1 Say hello 3times. counter outerFn

---
+++ js1/JS1.closure1.js
@@ -0,0 +1,21 @@
+const hello3x = () => {
+    let counter = 0
+    return () => {
+      if (counter === 3) {
+        return ""
+      }
+      counter = counter + 1
+      console.log("hello3x: counter = ", counter);
+          return "hello"
+    }
+  }
+console.log("hello3x = ", hello3x);
+
+const threeF = hello3x(); console.log("threeF = ", threeF);
+let b = threeF(); console.log("global: b = ", b); // b is "hello"
+b = threeF(); console.log("global: b = ",b);     // b is "hello"
+b = threeF(); console.log("global: b = ",b);     // b is "hello"
+b = threeF(); console.log("global: b = ",b);     // b is ""
+b = threeF(); console.log("global: b = ",b); // b will always be "" for all subsequent b = threeF() executions
+
+  
